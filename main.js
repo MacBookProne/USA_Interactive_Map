d9b90ed880f97aa84ec5c40d328af87edff33307
Justin O'Dea <MacBookProne@users.noreply.github.com>
2023-10-30 15:33:02 -0600

    Update main.js
    updated the script to function better.

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-$("path, circle").hover(function(e) {
-  $('#info-box').css('display','block');
-  $('#info-box').html($(this).data('info'));
-});
-
-$("path, circle").mouseleave(function(e) {
-  $('#info-box').css('display','none');
-});
-
-$(document).mousemove(function(e) {
-  $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
-  $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
-}).mouseover();
-
-var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-if(ios) {
-  $('a').on('click touchend', function() {
-    var link = $(this).attr('href');
-    window.open(link,'_blank');
-    return false;
-  });
-}
+<script>
+   jQuery(document).ready(function ($) {
+   	// When hovering over a path or circle
+   	$("path, circle").on('mouseenter', function () {
+   		$('#info-box').show().html($(this).data('info'));
+   	}).on('mouseleave', function () { // On mouse leave
+   		$('#info-box').hide();
+   	});
+   
+   	// Adjust the position of the info box based on the mouse movement
+   	$(document).on('mousemove', function (e) {
+   		$('#info-box').css({
+   			'top': e.pageY - $('#info-box').height() - 30,
+   			'left': e.pageX - ($('#info-box').width() / 2)
+   		});
+   	});
+   
+   	// Handle touch functionality for iOS devices
+   	var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+   	if (isIOS) {
+   		$('a').on('click touchend', function (e) {
+   			e.preventDefault(); // Prevent the default action
+   			var link = $(this).attr('href');
+   			window.open(link, '_blank');
+   		});
+   	}
+   });
+</script>
